119dc72923ba4fc5d3d53ad4e0a5a2fb2d662ad0
Ben Baron <ben@einsteinx2.com>
2021-11-27 00:18:36 -0500

    Change UART debug log pin to B7 to be compatible with dual nand mode

--- NANDway/include/uart_serial.h
+++ NANDway/include/uart_serial.h
@@ -31,10 +31,10 @@
 #define __UART_SERIAL_H__
 
 // change these to use another pin
-#define UART_TX_PORT PORTC
-#define UART_TX_PIN  PC7
-#define UART_TX_DDR  DDRC
-#define UART_TX_DDR_PIN DDC7
+#define UART_TX_PORT PORTB
+#define UART_TX_PIN  PB7
+#define UART_TX_DDR  DDRB
+#define UART_TX_DDR_PIN DDB7
 
 void UART_tx(char character);
 void UART_tx_str(char* string);
